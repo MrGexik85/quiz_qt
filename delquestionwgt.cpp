0437d4defd6f51e93cd17442775655043b1c1441
MeGexik85 <sem0156@mail.ru>
2021-09-28 15:27:15 +0300

    featch(delQuestionWgt): create base struct for Delete Question Widget clss

--- delquestionwgt.cpp
+++ delquestionwgt.cpp
@@ -1,6 +1,13 @@
 #include "delquestionwgt.h"
 
-DelQuestionWgt::DelQuestionWgt(QWidget *parent) : QWidget(parent)
+DelQuestionWgt::DelQuestionWgt(QWidget *parent, QString questFile) :
+    QWidget(parent), filename(questFile)
 {
 
+    qDebug() << "delQuestionWgt(constructor): Success";
+}
+
+DelQuestionWgt::~DelQuestionWgt() {
+
+    qDebug() << "delQuestionWgt(destructor): Success";
 }
--- delquestionwgt.h
+++ delquestionwgt.h
@@ -2,15 +2,19 @@
 #define DELQUESTIONWGT_H
 
 #include <QWidget>
+#include <QtWidgets>
 
 class DelQuestionWgt : public QWidget
 {
     Q_OBJECT
+private:
+    QString filename = "";
 public:
-    explicit DelQuestionWgt(QWidget *parent = nullptr);
-
+    explicit DelQuestionWgt(QWidget *parent = nullptr, QString questFile = "");
+    ~DelQuestionWgt();
 signals:
-
+    void backEvent();
+    void YuraGey();
 };
 
 #endif // DELQUESTIONWGT_H
