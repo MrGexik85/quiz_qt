6a29a84eec34ce2d2ab36f686400f5354bd7d5d6
MeGexik85 <sem0156@mail.ru>
2021-10-03 11:46:24 +0300

    featch(questionsModel): make base interface

--- questionsmodel.cpp
+++ questionsmodel.cpp
@@ -1,6 +1,101 @@
 #include "questionsmodel.h"
 
-QuestionsModel::QuestionsModel()
+QuestionsModel::QuestionsModel(QString filename) : filename(filename)
 {
-
+    opened = readDataFrom(filename);
+    qDebug() << "questionsModel(constructor): Success";
 }
+
+QuestionsModel::~QuestionsModel() {
+    saveDataTo(filename);
+    qDebug() << "questionsModel(destructor): Success";
+}
+
+bool QuestionsModel::isOpened() {
+    /*
+     *  Check that data of questions is valid
+     *
+     *  return: bool (data in not null)
+    */
+    return opened;
+}
+
+bool QuestionsModel::readDataFrom(QString filename) {
+    /* (in work)
+     *
+     *
+     *  return: bool (Success)
+    */
+
+    qDebug() << "questionsModel(readDataFrom): Success read from:" << filename;
+    return true;
+}
+
+bool QuestionsModel::saveDataTo(QString filename) {
+    /* (in work)
+     *
+     *
+     *  return: bool (Success)
+    */
+
+    qDebug() << "questionsModel(saveDataTo): Success save to:" << filename;
+    return true;
+}
+
+bool QuestionsModel::deleteRecord(int index) {
+    /* (in work)
+     *
+     *
+     * return: bool (success)
+    */
+    qDebug() << "questionsModel(deleteRecord): Success deleted from:" << index << "index";
+    return true;
+}
+
+bool QuestionsModel::addNewRecord(QString header, QVector<QString> answs, QVector<bool> rightAnsws) {
+    /* (in work)
+     *  Create Question object and save to data
+     *
+     *  return: bool (success)
+    */
+    qDebug() << "questionsModel(deleteRecord): Success added";
+    return true;
+}
+
+Question QuestionsModel::getQuestionData(int index) {
+    /*
+     *  Get Questions DS from index
+     *
+     *  return: Question
+    */
+    qDebug() << "qestionsModel(getQuestionData): Success";
+    return data[index];
+}
+
+QStringList QuestionsModel::getQuestionsHead() {
+    /*
+     *  Get list of question's text
+     *
+     *  return: QStringList
+    */
+
+    QStringList list;
+    for(int i = 0; i < data.size(); i++) {
+        list << data[i].questionHeader;
+    }
+
+    qDebug() << "questionsModel(getQuestionsHead): Success returned:" << list;
+    return list;
+}
+
+int QuestionsModel::size() {
+    /*
+     *  Get count of questions at now
+     *
+     *  return: int
+    */
+    return data.size();
+}
+
+
+
--- questionsmodel.h
+++ questionsmodel.h
@@ -1,11 +1,33 @@
 #ifndef QUESTIONSMODEL_H
 #define QUESTIONSMODEL_H
 
+#include <QtWidgets>
+
+struct Question {
+    QString questionHeader;
+    QVector<QString> answs;
+    QVector<bool> rightAnsws;
+};
 
 class QuestionsModel
 {
+private:
+    bool opened;
+    QString filename;
+    QVector<Question> data;
+
+    bool readDataFrom(QString filename);
+    bool saveDataTo(QString filename);
 public:
-    QuestionsModel();
+    QuestionsModel(QString filename);
+    ~QuestionsModel();
+
+    bool isOpened();
+    bool deleteRecord(int index);
+    bool addNewRecord(QString header, QVector<QString> answs, QVector<bool> rightAnsws);
+    QStringList getQuestionsHead();
+    Question getQuestionData(int index);
+    int size();
 };
 
 #endif // QUESTIONSMODEL_H
